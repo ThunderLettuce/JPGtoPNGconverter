bbbad927473b7b65b98fc9fc0a209154be20a715
Lettucee <pixelspammy@gmail.com>
2019-11-25 15:09:36 -0800

    Added Pokedex

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -1,12 +1,15 @@
-
-#! $python3 JPGtoNGconverter.py Pokedex\ new\
-
 import sys  # ? for terminal args
 import os  # ? set path
 from PIL import Image  # ? to convert the files
 
+#! $python3 JPGtoPNGconverter.py Pokedex\ new\
+
 
 # TODO grab first and second arguments
+JPG_folder = sys.argv[1]
+PNG_folder = sys.argv[2]
+
+print(JPG_folder, PNG_folder)
 
 # TODO check is new\ exists, if not create
 
