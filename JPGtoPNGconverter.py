0ae323e01785ed0c06f13586ae138f857afcb549
Lettucee <pixelspammy@gmail.com>
2019-11-25 15:48:09 -0800

    Add file navigation

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -1,4 +1,5 @@
 import sys  # ? for terminal args
+# from pathlib import Path
 import os  # ? set path
 from PIL import Image  # ? to convert the files
 
@@ -9,10 +10,16 @@ from PIL import Image  # ? to convert the files
 JPG_folder = sys.argv[1]
 PNG_folder = sys.argv[2]
 
-print(JPG_folder, PNG_folder)
-
 # TODO check is new\ exists, if not create
+if not os.path.exists(PNG_folder):
+    os.makedirs(PNG_folder)
+
 
 # TODO loop through pokedex
 # *      convert to PNG
 # *      save to the new folder
+
+for filename in os.listdir(JPG_folder):
+    img = Image.open(f'{JPG_folder}{filename}')
+    img.save(f'{PNG_folder}{filename}', 'png')
+    print('All Done.')
